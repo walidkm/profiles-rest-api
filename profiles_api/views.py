3176dc13c2e23b9d6f92be7a196dfbde610d5e04
Valid <whatsoffman@gmail.com>
2020-04-12 15:41:00 +0300

    Added viewsets

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -45,4 +46,17 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
-                
+
+
+class HelloApiViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+    """return a hello message"""
+    a_viewset = [
+        'Uses actions (list, create, retrieve, update, partial_update)',
+        'Automatically maps to URLs using Routers',
+        'Provides more functionality with less code',
+    ]
+
+    return Response({'message': 'Hello!', 'a_viewset': a_viewset})
